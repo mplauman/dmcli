96e46a8993662eb03e4891f230938da1c09cdd13
Matt Plaumann <matt@plaumann.ca>
2025-05-13 19:33:35 -0400

    Add event loop

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    println!("Hello, world!");
+    use tokio::sync::mpsc;
+    use tokio::task;
+
+    let (tx, mut rx) = mpsc::channel(32);
+
+    // Spawn a producer task to simulate adding tasks to the queue
+    task::spawn(async move {
+        for i in 1..=10 {
+            if tx.send(format!("Task {}", i)).await.is_err() {
+                println!("Receiver dropped");
+                break;
+            }
+        }
+    });
+
+    // Consumer loop
+    while let Some(task) = rx.recv().await {
+        println!("Processing {}", task);
+    }
 }
