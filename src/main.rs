54d9c8fb529b34b542d0a9587aba47a772f70c9f
Matt Plaumann <matt@plaumann.ca>
2025-05-16 11:43:17 -0400

    Minor refactors for readability

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,27 @@ use crate::errors::Error;
 mod commands;
 mod errors;
 
+fn read_line(readline: &mut rustyline::DefaultEditor) -> Result<Option<String>, Error> {
+    match readline.readline(">> ") {
+        Ok(line) => Ok(Some(line)),
+        Err(rustyline::error::ReadlineError::Interrupted) => Ok(None),
+        Err(x) => Err(x.into()),
+    }
+}
+
+fn parse(line: &str) -> Option<DmCommand> {
+    match split(line).map(DmCli::try_parse_from) {
+        Some(Ok(DmCli { command })) => Some(command),
+        _ => None,
+    }
+}
+
 fn main() -> Result<(), Error> {
     let mut rl = rustyline::DefaultEditor::new().unwrap();
 
     loop {
-        let line = match rl.readline(">> ") {
-            Ok(line) => line,
-            Err(rustyline::error::ReadlineError::Interrupted) => {
-                println!("Interrupted!");
-                break;
-            }
-            Err(x) => {
-                return Err(x.into());
-            }
+        let Some(line) = read_line(&mut rl)? else {
+            break;
         };
 
         if line.is_empty() {
@@ -28,9 +36,10 @@ fn main() -> Result<(), Error> {
 
         rl.add_history_entry(line.as_str())?;
 
-        if let Some(Ok(cli)) = split(line.as_str()).map(DmCli::try_parse_from) {
-            // They've entered some text that matches one of the configured commands. Run that directly since they'll know exactly what it is they want.
-            match cli.command {
+        // Check to see if the entered text can be handled with one of the built in commands. This
+        // is much faster, cheaper, and more accurate than feeding it to an AI agent for processing.
+        if let Some(command) = parse(line.as_str()) {
+            match command {
                 DmCommand::Exit {} => {
                     println!("Good bye!");
                     break;
@@ -39,11 +48,16 @@ fn main() -> Result<(), Error> {
                     println!("Rolling {:?}", expressions.join(" "));
                 }
             }
-        } else {
-            // They've entered some kind of handwavy text. Fire that off to the AI agent to figure
-            // out.
-            println!(">>> {}", line);
+
+            continue;
         }
+
+        // Anything that isn't specifically a command just gets fed to the AI agent. This can take
+        // a lot longer to deal with (and is more expensive) but is where the magic like "create an
+        // NPC with blue hair" type stuff works.
+        //
+        // (until the agent actually works, just spit out the line)
+        println!(">>> {}", line);
     }
 
     Ok(())
