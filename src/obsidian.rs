5ad14f002bacfd0f4030fb11a724cf534ff35885
Matt Plaumann <matt@plaumann.ca>
2025-05-19 21:07:58 -0400

    Ability to read markdown files

--- src/obsidian.rs
+++ src/obsidian.rs
@@ -43,24 +43,52 @@ impl LocalVault {
 
         serde_json::json!(files)
     }
+
+    fn read_markdown_file(&self, params: &serde_json::Value) -> serde_json::Value {
+        let path = params["path"].as_str().expect("path is a string");
+        let path = self.vault.join(path);
+        let contents = std::fs::read_to_string(path).expect("failed to read file");
+
+        serde_json::json!({
+            "contents": contents,
+        })
+    }
 }
 
 impl Toolkit for LocalVault {
     fn list_tools(&self) -> Vec<Tool> {
-        vec![Tool {
-            name: "get_session_notes",
-            description: "Returns a list of the dungeon master's local notes",
-            input_schema: serde_json::json!({
-                "type": "object",
-                "parameters": {},
-                "required": [],
-            }),
-        }]
+        vec![
+            Tool {
+                name: "get_session_notes",
+                description: "Returns a list of the dungeon master's local notes",
+                input_schema: serde_json::json!({
+                    "type": "object",
+                    "parameters": {},
+                    "required": [],
+                }),
+            },
+            Tool {
+                name: "read_markdown_file",
+                description: "Return the contents of a markdown file",
+                input_schema: serde_json::json!({
+                    "type": "object",
+                    "properties": {
+                        "path": {
+                            "type": "string",
+                            "description": "The path to the markdown file",
+                        },
+                    },
+                    "required": ["path"],
+                    "additionalProperties": false,
+                }),
+            },
+        ]
     }
 
-    fn run_tool(&self, name: &str, _: &serde_json::Value) -> serde_json::Value {
+    fn run_tool(&self, name: &str, params: &serde_json::Value) -> serde_json::Value {
         match name {
             "get_session_notes" => self.list_vault_contents(),
+            "read_markdown_file" => self.read_markdown_file(params),
             _ => panic!("don't know how to handle {}", name),
         }
     }
